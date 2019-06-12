0ba39dfba72bf114c55a9404c32ae8a674d2165b
saeid4x <saeid.almahdi@gmail.com>
2019-06-12 06:46:17 +0430

    change keyCode for backend url

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,5 +1,5 @@
 export default {
-    backendUrl:'http://127.0.0.1:8000/',
+    backendUrl:'https://project-customer.herokuapp.com/',
     pattern:{
         email:/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
         mobile:/^09\d{9}$/,
